21de049504b6c531841843bad31cfaacc51d97e8
kaptsov <kaptsovaa@gmail.com>
2022-05-06 00:36:45 +0400

    Добавлен line владельца по квартире. ФИО отображается только 1 раз (?)

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Disliker, Owner
 
 
+class OwnersInline(admin.TabularInline):
+    model = Flat.owners.through
+    raw_id_fields = ['owner']
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
     readonly_fields = ['created_at']
@@ -10,6 +15,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ['new_building']
     raw_id_fields = ['liked_by']
+    inlines = [OwnersInline]
 
 
 class DislikeAdmin(admin.ModelAdmin):

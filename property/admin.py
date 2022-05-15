84151bb94aeb284e3baa733315e0f535e9ac0bbb
kaptsov <kaptsovaa@gmail.com>
2022-05-15 21:38:09 +0400

    Попробован декоратор "@register"

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,7 @@ class OwnersInline(admin.TabularInline):
     model = Flat.owners.through
     raw_id_fields = ['owner']
 
-
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
     readonly_fields = ['created_at']
@@ -17,15 +17,10 @@ class FlatAdmin(admin.ModelAdmin):
     raw_id_fields = ['liked_by']
     inlines = [OwnersInline]
 
-
+@admin.register(Disliker)
 class DislikeAdmin(admin.ModelAdmin):
     raw_id_fields = ['disliker', 'flat']
 
-
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ['flats']
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Disliker, DislikeAdmin)
-admin.site.register(Owner, OwnerAdmin)

10ceac529b0bdfbb8706841e3f7a6786fc8f8500
kaptsov <kaptsovaa@gmail.com>
2022-05-15 21:27:49 +0400

    Добавлены verbose_names и related_names

--- property/models.py
+++ property/models.py
@@ -7,21 +7,24 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Flat(models.Model):
 
-    new_building = models.BooleanField('Новостройка', default=False, null=True, db_index=True)
+    new_building = models.BooleanField(verbose_name='Новостройка',
+        default=False,
+        null=True,
+        db_index=True)
     created_at = models.DateTimeField(
-        'Когда создано объявление',
+        verbose_name='Когда создано объявление',
         default=timezone.now,
         db_index=True)
 
-    description = models.TextField('Текст объявления', blank=True)
-    price = models.IntegerField('Цена квартиры', db_index=True)
+    description = models.TextField(verbose_name='Текст объявления', blank=True)
+    price = models.IntegerField(verbose_name='Цена квартиры', db_index=True)
 
     town = models.CharField(
-        'Город, где находится квартира',
+        verbose_name='Город, где находится квартира',
         max_length=50,
         db_index=True)
     town_district = models.CharField(
-        'Район города, где находится квартира',
+        verbose_name='Район города, где находится квартира',
         max_length=50,
         blank=True,
         help_text='Чертаново Южное')
@@ -34,30 +37,39 @@ class Flat(models.Model):
         help_text='Первый этаж, последний этаж, пятый этаж')
 
     rooms_number = models.IntegerField(
-        'Количество комнат в квартире',
+        verbose_name='Количество комнат в квартире',
         db_index=True)
     living_area = models.IntegerField(
-        'количество жилых кв.метров',
+        verbose_name='количество жилых кв.метров',
         null=True,
         blank=True,
         db_index=True)
 
-    has_balcony = models.NullBooleanField('Наличие балкона', db_index=True)
-    active = models.BooleanField('Активно-ли объявление', db_index=True)
+    has_balcony = models.NullBooleanField(verbose_name='Наличие балкона', db_index=True)
+    active = models.BooleanField(verbose_name='Активно-ли объявление', db_index=True)
     construction_year = models.IntegerField(
-        'Год постройки здания',
+        verbose_name='Год постройки здания',
         null=True,
         blank=True,
         db_index=True)
-    liked_by = models.ManyToManyField(User, related_name="liked_apt", blank=True)
+    liked_by = models.ManyToManyField(User, related_name="liked_flat", blank=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
 
 
 class Disliker(models.Model):
-    disliker = models.ForeignKey(User, on_delete=models.CASCADE)
-    flat = models.ForeignKey(Flat, on_delete=models.CASCADE)
+
+    disliker = models.ForeignKey(
+        User,
+        related_name='dislikers',
+        verbose_name='Кому не понравилась квартира',
+        on_delete=models.CASCADE)
+    flat = models.ForeignKey(
+        Flat,
+        related_name='dislikers',
+        verbose_name='Квартира, которая не понравилась',
+        on_delete=models.CASCADE)
     message = models.TextField(blank=False)
 
     def __str__(self):
@@ -66,12 +78,12 @@ class Disliker(models.Model):
 
 class Owner(models.Model):
     fullname = models.CharField(
-        'ФИО владельца',
+        verbose_name='ФИО владельца',
         max_length=200,
         db_index=True)
-    phonenumber = models.CharField('Номер владельца', max_length=20)
+    phonenumber = models.CharField(verbose_name='Номер владельца', max_length=20)
     phonenumber_normalized = PhoneNumberField(
-        'Нормализованный номер владельца',
+        verbose_name='Нормализованный номер владельца',
         null=True,
         blank=True
     )
